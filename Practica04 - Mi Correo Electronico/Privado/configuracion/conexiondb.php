c7fe56f276f44d9456b345c678ba8cc0a4f2bc26
AngelJadan <angel.jadan12@gmail.com>
2019-05-12 16:24:38 -0500

    actualizacion vistas

--- Practica04 - Mi Correo Electronico/Privado/configuracion/conexiondb.php
+++ Practica04 - Mi Correo Electronico/Privado/configuracion/conexiondb.php
@@ -1,3 +1,15 @@
 <?php
+$db_servername="localhost";
+$db_username="root";
+$db_password="";
+$db_name="siscorreodb";
+
+$conn=new mysqli($db_servername,$db_username,$db_password,$db_name);
+
+if ($conn->connect_error){
+    die("Conexión fallida!! ".$conn->connect_error);
+}else{
+    echo"<p>Conexion exitosa</p>";
+}
 
 ?>
