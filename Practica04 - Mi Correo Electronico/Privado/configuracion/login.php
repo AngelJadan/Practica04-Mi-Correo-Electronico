194c84375dd36ffa80b8a48dd304ce6c3f772edc
AngelJadan <angel.jadan12@gmail.com>
2019-05-12 16:34:43 -0500

    actualizacion vistas

--- Practica04 - Mi Correo Electronico/Privado/configuracion/login.php
+++ Practica04 - Mi Correo Electronico/Privado/configuracion/login.php
@@ -1,2 +1,17 @@
 <?php
+    session_start();
+    
+    include '/conexiondb.php'
+    $usuario = isset($_POST["usuario"])?trim($_POST["correo"]):null;
+    $contrasena = isset($_POST["contrasena"])?trim($_POST["contrasena"]):null;
+
+    $sql="SELECT * FROM usuario where usu_correo='$usuario' and usu_contrasena = MD5('$contrasena')";
+    $result = $conn->query($sql);
+    if($result->num_rows>0){
+        $_SESSION['isLogged']=TRUE;
+        header("Location:..            falta terminar direccion  ")
+    }else{
+        header("Location:../index.html")
+    }
+    $conn->close();
 ?>
